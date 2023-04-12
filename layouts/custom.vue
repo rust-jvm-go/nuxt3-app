b777d28ea67a59a7382b98dc7db91ea9afbe8d96
rustxjvmxgo <rustxjvmxgo@gmail.com>
2023-04-13 00:22:12 +0800

    Added custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="CustomLayout h-screen text-white bg-blue-900">
+    <!-- This default layout will be applied to the whole site. -->
+    <p>Custom layout</p>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "custom"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/custom.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="CustomPage">Custom page</div>
+</template>
+
+<script>
+export default {
+  name: "custom",
+  mounted() {
+    definePageMeta({
+      layout: "custom",
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
